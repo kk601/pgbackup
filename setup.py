ff0954b403e09f2fc55644192ee2d97161cb13d9
kk601 <kubakurowski106@gmail.com>
2022-02-19 16:20:06 +0100

    Structure project with setup.py and makefile

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='UTF-8') as f:
+    readname = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='database backups locally or to azure storeage',
+    author='kk601',
+    author_email='kubakurowski106@gmail.com',
+    install_requires=[],
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
